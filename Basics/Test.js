8a25e3bc4cc08e92cbef3b129155953587c5968b
naveenkumar9829 <134676168+naveenkumar9829@users.noreply.github.com>
2025-09-11 18:30:06 +0000

    variables and datatypes

--- Basics/Test.js
+++ Basics/Test.js
@@ -1,1 +1,42 @@
+
+"use strict";
+/*
 console.log("naveen")
+
+const Gender="Male"
+var Name = "Naveen yogi"
+let Disignation = "Software engineer"
+var Addrsh;
+
+ Gender="FeMale" 
+Name = "Amit Yogi"
+Disignation = "AI engineer"
+console.log(Gender)
+console.log(Name)
+console.log(Disignation)
+console.log(Addrsh) 
+
+console.table([Gender,Name,Disignation,Addrsh])
+
+*/
+
+//.......................................................................
+
+/*
+// data types ........
+var Roll_no = 23           // number
+let Name = "Naveen"        // string
+let AccountNo = 5838375835394595463  //number
+let FatherName;                //undefined
+let motherName =null           //object
+let is_male=true               //boolen
+
+console.log(typeof Roll_no)   
+console.log(typeof Name)
+console.log(typeof AccountNo)
+console.log(typeof FatherName)
+console.log(typeof is_male)
+console.log(typeof motherName)
+console.log(typeof is_male)
+*/
+
