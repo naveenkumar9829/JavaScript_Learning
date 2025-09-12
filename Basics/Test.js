0f6b6b75f4b2d131c6313a960f5cde105ad123dc
naveenkumar9829 <134676168+naveenkumar9829@users.noreply.github.com>
2025-09-12 07:04:21 +0000

    Compairrison

--- Basics/Test.js
+++ Basics/Test.js
@@ -40,3 +40,157 @@ console.log(typeof motherName)
 console.log(typeof is_male)
 */
 
+//************************************************************************************************** 
+
+// change in the String..........
+
+/* var Roll_no = 23 
+console.log(typeof Roll_no)
+
+let change_element = (String(Roll_no))
+console.log(change_element)
+console.log(typeof change_element)  */
+
+
+
+// var Roll_no = true
+// console.log(typeof Roll_no)
+
+// let change_element = (String(Roll_no))
+// console.log(change_element)
+// console.log(typeof change_element)
+
+
+
+// change in the Number..........
+
+// var Name=""    // it is 0
+// var Name="Naveen"     // in Number it is NaN
+// var Name="89"          // in int it is 89
+
+
+
+// var Name;     //Nan
+//var Name=null   
+
+// var Name = false
+// console.log(typeof Name)   
+// let change_element = (Number(Name))
+// console.log(change_element)
+// console.log(typeof change_element)
+
+
+
+// change in the  Boolean...................
+   
+
+//var Name = "NAVEEN"  // true
+//var Name=null        // false
+//var Name=""           // false
+
+//var Name=67   // True
+// var Name = 90.90 // True      
+// var Name;  //False
+
+
+// console.log(typeof(Name)) 
+// let change_element = (Boolean(Name))   // Flase
+// console.log(change_element) 
+// console.log(typeof(change_element))
+
+// var Name=null     
+// console.log(typeof(Name)) 
+// let change_element = (Number(Name))  // 0
+// console.log(change_element)
+// console.log(typeof(change_element))
+
+
+//..............................................
+
+// let num1=2
+// let num2=2
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
+// console.log(num1**num2)
+
+
+// let num1=2
+// let num2="Naveen"
+// let num3=1
+
+// let result=num2+num1+num3
+// console.log(result)
+// console.log(typeof result)
+
+// let name =(90+"naveen")
+// console.log(typeof name)
+
+
+// let number = 100
+// console.log(number++)
+
+
+// let num1= 40
+// let num2= ""
+
+// console.log(typeof(num1+num2))
+
+
+//*******************************************************  
+
+
+
+// let name1 = "Naveen"
+// let name2= "abc"
+
+// console.log(name1==name2)
+// console.log(name1>name2)  // false
+// console.log(name1<name2)  // True
+
+
+// let name1 = "0"      
+// let name2= true
+// //console.log(name1==name2) // false
+// console.log(name1<name2)    // false 
+
+
+// let name = "Naveen"  // it is Nan
+// let Roll_no = 45
+
+// console.log(name<Roll_no)
+
+// console.log(true<0)
+
+
+
+// let num1 = null
+// let num2 = 0
+
+// console.log(num1==num2)
+// console.log(num1>num2)
+// console.log(num1<num2)
+// console.log(num1>=num2)
+// console.log(num1<=num2)
+
+
+// let num1 = undefined   // it is not convert into the number it is stay in NaN
+// let num2 = 0
+
+// console.log(num1==num2)
+// console.log(num1>num2)
+// console.log(num1<num2)
+// console.log(num1>=num2)
+// console.log(num1<=num2)
+
+
+
+// let Name1 = "2"
+// let Name2 = 2
+// console.log(Name1==Name2)
+
+
+
